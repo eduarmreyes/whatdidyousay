5a79fb660f47f11c9439bb33a06da6437edfa706
eduardo.mejia <eduardo.mejia@telusinternational.com>
2014-10-24 16:31:36 -0600

    Transitions using angular and jQuery, good stuf!

--- js/angular_controllers.js
+++ js/angular_controllers.js
@@ -35,13 +35,73 @@ var aStatBoxes = [
 ];
 var aQuestions = [
 	{
-		question: "What is the first person of the pronound",
+		idElement: "Q1",
+		question: "What is the first person of the pronound?",
 		answeres: {
-			first: "I",
-			second: "You"
+			first: {
+				text: "I",
+				correct: true,
+				target: "#Q1",
+				next: "#Q2"
+			},
+			second: {
+				text: "You",
+				correct: false
+			}
 		},
 		correctAnswer: 1
-	}
+	},
+	{
+		idElement: "Q2",
+		question: "How many WH-questions are?",
+		answeres: {
+			first: {
+				text: "8",
+				correct: false
+			},
+			second: {
+				text: "9",
+				correct: true,
+				target: "#Q2",
+				next: "#Q3"
+			}
+		},
+		correctAnswer: 1
+	},
+	{
+		idElement: "Q3",
+		question: "Select the correct preposition inside parenthesis: I'm working (to/at) McDonald's",
+		answeres: {
+			first: {
+				text: "to",
+				correct: true,
+				target: "#Q3",
+				next: "#Q4"
+			},
+			second: {
+				text: "at",
+				correct: false
+			}
+		},
+		correctAnswer: 1
+	},
+	{
+		idElement: "Q4",
+		question: "Do you like this game?",
+		answeres: {
+			first: {
+				text: "Yes",
+				correct: true,
+				target: "#Q4",
+				next: "#Q5"
+			},
+			second: {
+				text: "No",
+				correct: false
+			}
+		},
+		correctAnswer: 1
+	},
 ];
 (function() {
 	var app = angular.module("whatdidyousay", []);
